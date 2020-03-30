33b15bda56d776b65b4baff44f5b18ca58ad543c
David Ison <dison@combinedpublic.com>
2020-03-30 15:47:37 -0400

    Arranges for localhost signaling server.

--- run-signaling-server.sh
+++ run-signaling-server.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-tmux new -s SIGNALING sudo  /home/disondev/.nvm/versions/node/v13.12.0/bin/node signaling-server.js  
+#tmux new -s SIGNALING sudo  /home/disondev/.nvm/versions/node/v13.12.0/bin/node signaling-server.js  
+tmux new -s SIGNALING  node signaling-server.js
+# node signaling-server.js
